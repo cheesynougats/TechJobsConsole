2d48ce2285a26d88c040fe238daa075797f59505
Wade <cheesynougats@gmail.com>
2017-03-09 20:59:30 -0600

    Finish assignment

--- src/TechJobsConsole/JobData.cs
+++ src/TechJobsConsole/JobData.cs
@@ -10,6 +10,23 @@ namespace TechJobsConsole
         static List<Dictionary<string, string>> AllJobs = new List<Dictionary<string, string>>();
         static bool IsDataLoaded = false;
 
+        public static List<Dictionary<string, string>> FindByValue(string search)
+        {
+            LoadData();
+            List<Dictionary<string, string>> results = new List<Dictionary<string, string>>();
+            foreach (Dictionary<string,string> job in AllJobs)
+            {
+                foreach (KeyValuePair<string,string> field in job)
+                {
+                    if (field.Value.ToLower().Contains(search.ToLower()))
+                    {
+                        results.Add(job);
+                        break;
+                    }
+                }
+            }
+            return results; 
+        }
         public static List<Dictionary<string, string>> FindAll()
         {
             LoadData();
@@ -49,7 +66,7 @@ namespace TechJobsConsole
             {
                 string aValue = row[column];
 
-                if (aValue.Contains(value))
+                if (aValue.ToLower().Contains(value.ToLower()))
                 {
                     jobs.Add(row);
                 }
